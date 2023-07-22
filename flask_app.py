cd5488bcc2c1d53b24f528fb5aa79a766aa5c9f6
jeremiah <jeremiahdlk@gmail.com>
2023-07-22 23:01:02 +0000

    added database to website

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="jered",
+    password="HelloWorld!23",
+    hostname="jered.mysql.pythonanywhere-services.com",
+    databasename="jered$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy (app)
 
 comments = []
 
